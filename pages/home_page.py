9d08202bda63166ac4bf6cb49a092b14d4fbb116
Mostafa Damir <mad237@njit.edu>
2022-05-16 19:34:42 -0400

    added calculate and time or distance function

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,6 +1,7 @@
 
 from cgitb import text
 from re import A
+from tkinter import Button
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
@@ -8,6 +9,11 @@ import pandas as pd
 
 layout = html.Div(
     [
+    html.Hgroup(children = 'Prioritize'),
+    dcc.Checklist(
+    ['Time', 'Distance'],
+    inline=True
+    ),
     dcc.Input(
             
             id="input_Origin",
@@ -23,11 +29,15 @@ layout = html.Div(
             placeholder = "Stops"
 
         ),
+        # html.Button('Add another Stop'),
+        # html.Div(id='container-button-basic'),
         dcc.Input(
             id ="input_Destination",
             type = "text",
             placeholder = "Destination"
 
-        )
+        ),
+    html.Button('Calculate'),
+    html.Div(id='container-button-basic')
     ]
 )
