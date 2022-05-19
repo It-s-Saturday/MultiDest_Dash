0c6951e026e07403ae39d26c3fb510fe0b0a92f0
Ding13028 <87954855+Ding13028@users.noreply.github.com>
2022-05-19 12:14:27 -0400

    add header

--- pages/aboutpage.py
+++ pages/aboutpage.py
@@ -4,6 +4,22 @@ import plotly.express as px
 import pandas as pd
 import base64
 import dash_html_components as html
+Div = html.Div([
+    html.H1('What is MultiDest'),
+    html.Div([ 
+        html.P('Multidest is a new way for people to get '),
+        html.P('....... !!!!'),
+        html.P(''),
+    ]),
+
+    html.H2('How does it work'),
+    html.Div([ 
+        html.P('.......'),
+        html.P('........'),
+    ])
+    
+])  
+
 
 image_filename = os.getcwd()+'/pages/AM.png'  # replace with your own image
 print(image_filename)
@@ -13,7 +29,6 @@ def b64_image(image_filename):
         image = f.read()
     return 'data:image/png;base64,' + base64.b64encode(image).decode('utf-8')
 
-layout = html.Img(src=b64_image(image_filename))
-
-
+image = html.Img(src=b64_image(image_filename))
+layout = html.Div([image, Div])
 
