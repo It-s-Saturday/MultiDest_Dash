37244e67381c8aa2573321b6f4a3e08bf15fdf07
jibranabsarulislam <jga26@njit.edu>
2022-05-21 13:05:10 -0400

    optimize all_paths

--- models/GoogleApi.py
+++ models/GoogleApi.py
@@ -1,18 +1,29 @@
 class GoogleApi:
     def __init__(self):
-        with open('key.txt', 'r') as filename:  # open the key.txt file on local machine
+        with open("key.txt", "r") as filename:  # open the key.txt file on local machine
             self.api_key = filename.read()  # set api_key to file content
-        
+
         self.gmaps = googlemaps.Client(key=api_key)  # initialize googlemaps client
+        print("OK")
 
     def lookup(self, origin_in, destination_in, mode, choice):
         """Uses the GoogleMaps API to retrieve a JSON that contains the duration and distance between the origin_in and
-        destination_in) """
+        destination_in)"""
         # print("Start lookup:", origin_in, destination_in, "with", mode, choice)
-        origin = origin_in.replace(' ', '+')
-        destination = destination_in.replace(' ', '+')
-        url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&'
-        concat = url + "origins=" + origin + "&destinations=" + destination + "&key=" + api_key + "&mode=" + mode
+        origin = origin_in.replace(" ", "+")
+        destination = destination_in.replace(" ", "+")
+        url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
+        concat = (
+            url
+            + "origins="
+            + origin
+            + "&destinations="
+            + destination
+            + "&key="
+            + api_key
+            + "&mode="
+            + mode
+        )
         r = requests.get(concat)
         measurement = r.json()["rows"][0]["elements"][0][choice]["text"]
 
