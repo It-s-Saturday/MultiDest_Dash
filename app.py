bcf13410e0d0287fda95b2006f2c951221ed036d
jibranabsarulislam <jga26@njit.edu>
2022-05-27 00:12:19 -0400

    replace app init timer benchmarker

--- app.py
+++ app.py
@@ -4,23 +4,22 @@ import time
 import dash_bootstrap_components as dbc
 from dash import Dash, html
 
+from models.Timer import Timer
 from callbacks import homepage_callbacks, sidebar_callbacks
 from layout import CONTENT_STYLE, layout
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.title = "MultiDest"
+with Timer("App Init") as t:
+    app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+    app.title = "MultiDest"
 
-print("rendering...")
-server = app.server
-start = time.time()
+    server = app.server
+    start = time.time()
 
-app.layout = html.Div(
-    [
-        layout,
-    ],
-    style=CONTENT_STYLE,
-)
-print(f"render complete {round(time.time() - start, 2)}s)")
-
+    app.layout = html.Div(
+        [
+            layout,
+        ],
+        style=CONTENT_STYLE,
+    )
 if __name__ == "__main__":
     app.run_server(debug=True)
