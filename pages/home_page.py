1d079cf7d06d07e899e2746bfea03d8b1677e9e8
jibranabsarulislam <jga26@njit.edu>
2022-05-26 23:31:47 -0400

    divvy up components

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,23 +3,60 @@ import dash_bootstrap_components as dbc
 
 layout = html.Div(
     [
-        html.Hgroup(children="Prioritize"),
-        dcc.RadioItems(
-            id="input-metric", options=["Time", "Distance"], value="Time", inline=True
-        ),
-        dcc.Input(id="input-origin", type="text", placeholder="Origin"),
         html.Div(
-            id="stops",
-            children=[
-                dcc.Input(id="stop-2", type="text", placeholder="Stop 1"),
-                dcc.Input(id="stop-1", type="text", placeholder="Stop 2"),
+            [
+                dbc.Label("Calculate"),
+                dbc.RadioItems(
+                    id="input-metric",
+                    options=[
+                        {"label": "Time", "value": "time"},
+                        {"label": "Distance", "value": "distance"},
+                    ],
+                    value="time",
+                    inline=True,
+                ),
             ],
+            id="radio-container",
         ),
-        dcc.Input(id="input-destination", type="text", placeholder="Destination"),
-        html.Button("Calculate", id="btn-calculate"),
-        html.Div(id="container-button-basic"),
-        html.Button("Add stop", id="add-stop"),
-        html.Button("Remove Stop", id="remove-stop"),
-        dbc.Spinner(html.Div(id="output"), delay_hide=100, delay_show=100, show_initially=False),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        dbc.Label("Origin"),
+                        dbc.Input(id="input-origin", type="text", placeholder="Origin"),
+                    ]
+                ),
+                html.Div(
+                    id="stops",
+                    children=[
+                        dbc.Input(id="stop-1", type="text", placeholder="Stop 1"),
+                        dbc.Input(id="stop-2", type="text", placeholder="Stop 2"),
+                    ],
+                ),
+                dbc.Input(
+                    id="input-destination", type="text", placeholder="Destination"
+                ),
+            ],
+            id="input-container",
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        dbc.Button("Add stop", color="secondary", id="add-stop"),
+                        dbc.Button("Remove Stop", color="secondary", id="remove-stop"),
+                    ],
+                    id="add-remove-button-container",
+                ),
+                dbc.Button("Calculate", id="btn-calculate"),
+                dbc.Spinner(
+                    html.Div(id="output"),
+                    delay_hide=100,
+                    delay_show=100,
+                    show_initially=False,
+                ),
+            ],
+            id="button-container",
+        ),
     ]
 )
